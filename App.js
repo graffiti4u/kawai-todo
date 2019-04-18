2ecf850b7a079a2482b405c2e478270298b00f14
Graffiti <graffiti4u@naver.com>
2019-04-18 11:12:13 +0900

    <Platform>을 이용하여 ios와 android에서의 코딩 분리.

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   StatusBar,
   TextInput,
   Dimensions,
+  Platform,
 } from 'react-native';
 
 const { height, width } = Dimensions.get('window');
@@ -35,10 +36,27 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginTop: 50,
     fontWeight: '200',
+    marginBottom: 30,
   },
   card: {
     backgroundColor: 'white',
     flex: 1,
     width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0,
+        },
+      },
+      android: {
+        elevation: 3,
+      },
+    }),
   },
 });
