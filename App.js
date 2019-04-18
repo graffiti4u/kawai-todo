ebdb29d6d8edfb2530f545592a4adc536df63bc9
Graffiti <graffiti4u@naver.com>
2019-04-18 10:48:55 +0900

    기본 디자인 만들기.
    1. <StatusBar> 이용법 습득.
    2. <TextInput> 이용법 습득.
    3. <Dimenesions>를 이용하여 플랫폼 크기 알아내기.

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+} from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>kawai To Do</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={'New To Do'} />
+        </View>
       </View>
     );
   }
@@ -14,8 +27,18 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: '200',
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width - 25,
   },
 });
