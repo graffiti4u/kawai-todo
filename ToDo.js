76f3979cc7f69d2cc5bdf8f99c8bc80b8b52dcbe
Graffiti <graffiti4u@naver.com>
2019-04-21 07:56:29 +0900

    static으로 propTypes 지정

--- ToDo.js
+++ ToDo.js
@@ -7,13 +7,18 @@ import {
   Dimensions,
   TextInput,
 } from 'react-native';
+import PropTypes from 'prop-types';
 
 const { width, height } = Dimensions.get('window');
 
 export default class ToDo extends Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    isCompleted: PropTypes.bool.isRequired,
+  };
+
   state = {
     isEditing: false,
-    isCompleted: false,
     toDoValue: '',
   };
 
